1c57c602688ce086ee095fa33f2cbb658735b9c0
Leonardo Lazzaro <lazzaroleonardo@mindpixel.com.ar>
2013-06-16 01:29:50 -0300

    database-populate now uses new model

--- commands/database.py
+++ commands/database.py
@@ -1,17 +1,30 @@
 import json
-from pymongo import MongoClient
+import logging
 
-client = MongoClient()
-db = client.lenin_database
+from pymongo.errors import DuplicateKeyError
+
+from information_value.models import odm_session
+from information_value.models import Document
+
+
+log= logging.getLogger('lenin')
 
 
 def populate_database():
-    print "Populating database..."
-    works = db.works
+    log.info("Populating database...")
     with file('lenin_work.json') as f:
         raw_works = json.load(f)
-        print "Inserting %s works to database..." % (len(raw_works))
+        log.info("Inserting %s works to database..." % (len(raw_works)))
         for raw_work in raw_works:
-            works.insert(raw_work)
-        print "Done"
-
+            try:
+                Document(
+                    url=raw_work['url'],
+                    text=raw_work['text'],
+                    name=raw_work['name'],
+                    month=raw_work['month'],
+                    year=int(raw_work['year'])
+                )
+                odm_session.flush()
+            except DuplicateKeyError:
+                log.info("Duplicate found skipping...")
+        log.info("Done")
