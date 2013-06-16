9c7ff0dca701480696c24f29816730f511b1eae5
Leonardo Lazzaro <lazzaroleonardo@mindpixel.com.ar>
2013-06-16 01:29:03 -0300

    add lenin logger

--- cccp.py
+++ cccp.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
-import unittest
 import config
+import logging
+import unittest
 import argparse
 import subprocess
 from commands.database import populate_database
@@ -82,5 +83,23 @@ def main():
     if args.test:
         run_tests()
 
+def init_logging():
+    logger = logging.getLogger('lenin')
+    logger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler('lenin.log')
+    fh.setLevel(logging.DEBUG)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.ERROR)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
 if __name__ == "__main__":
+    init_logging()
     main()
