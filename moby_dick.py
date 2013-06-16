034692b434ab2593fdb2b4f7b4307e73525105a7
Leonardo Lazzaro <lazzaroleonardo@mindpixel.com.ar>
2013-06-16 01:30:24 -0300

    moby dick script now uses new model

--- moby_dick.py
+++ moby_dick.py
@@ -1,34 +1,52 @@
 from __future__ import division
 from nltk.corpus import gutenberg
-from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
 from includes.tokenizer import tokenize
 from information_value.analysis import get_optimal_window_size
 
-client = MongoClient()
-db = client.moby_dick_database
 
-
+from information_value.models import odm_session
+from information_value.models import InformationValueResult
+from information_value.models import Document
 
 window_sizes = xrange(100, 6000, 100)
 sum_thresholds = [0.0005, 0.001, 0.002, 0.003, 0.005, 0.006, 0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.2, 0.3 ]
 
 
 def load_moby_dick_analysis():
-    db.drop_collection('analysis')
     tokens = get_moby_dick_tokens()
-    analysis_collection = db.analysis
+    text = gutenberg.raw('melville-moby_dick.txt')
+    try:
+        moby_dick_doc = Document(
+            url='gutenberg',
+            name='moby dick',
+            text=text,
+            month='Jan',
+            year=1851
+            )
+        odm_session.flush()
+    except DuplicateKeyError:
+        moby_dick_doc = Document.query.get(name='moby dick')
+
     for sum_threshold in sum_thresholds:
         print "Trying analysis for threshold = %s" % sum_threshold
         window_size, analysis = get_optimal_window_size(tokens, window_sizes, 20, sum_threshold=sum_threshold)
-        print analysis
-        analysis_collection.insert({
-            'sum_threshold': sum_threshold,
-            'analysis': analysis.encode()
-            })
-            
-    
+        anal_dict = analysis.encode()
+        InformationValueResult(
+            window_size = window_size,
+            threshold = sum_threshold,
+            document = moby_dick_doc,
+            iv_words = anal_dict['top_words'],
+            max_iv = anal_dict['max_iv'],
+            sum_iv = anal_dict['sum_iv']
+        )
+        odm_session.flush()
+
 
 def get_moby_dick_tokens():
     moby_dick = gutenberg.raw('melville-moby_dick.txt')
     tokens = tokenize(moby_dick, only_alphanum=True, clean_punctuation=True)
     return [token.lower() for token in tokens]
+
+if __name__ == '__main__':
+    load_moby_dick_analysis()
