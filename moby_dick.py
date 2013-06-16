d1d042ebb8573577cb424567a7ea3a764b9e2f48
Leonardo Lazzaro <lazzaroleonardo@mindpixel.com.ar>
2013-06-16 17:48:02 -0300

    remove prints use logging instead

--- moby_dick.py
+++ moby_dick.py
@@ -1,14 +1,17 @@
 from __future__ import division
+import logging
 from nltk.corpus import gutenberg
 from pymongo.errors import DuplicateKeyError
+
+from cccp import init_logging
 from includes.tokenizer import tokenize
 from information_value.analysis import get_optimal_window_size
-
-
 from information_value.models import odm_session
 from information_value.models import InformationValueResult
 from information_value.models import Document
 
+log= logging.getLogger('lenin')
+init_logging()
 window_sizes = xrange(100, 6000, 100)
 sum_thresholds = [0.0005, 0.001, 0.002, 0.003, 0.005, 0.006, 0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.2, 0.3 ]
 
@@ -29,7 +32,7 @@ def load_moby_dick_analysis():
         moby_dick_doc = Document.query.get(name='moby dick')
 
     for sum_threshold in sum_thresholds:
-        print "Trying analysis for threshold = %s" % sum_threshold
+        log.info("Trying analysis for threshold = %s" % sum_threshold)
         window_size, analysis = get_optimal_window_size(tokens, window_sizes, 20, sum_threshold=sum_threshold)
         anal_dict = analysis.encode()
         InformationValueResult(
