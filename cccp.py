2042402ec8e687665f3238248c4c2eb9d552b87d
geekazoid <jmperez.85@gmail.com>
2013-06-23 14:47:13 -0300

    Changed level of console handler.

--- cccp.py
+++ cccp.py
@@ -91,7 +91,7 @@ def init_logging():
     fh.setLevel(logging.DEBUG)
     # create console handler with a higher log level
     ch = logging.StreamHandler()
-    ch.setLevel(logging.ERROR)
+    ch.setLevel(logging.DEBUG)
     # create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
