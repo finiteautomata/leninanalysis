0b2c0eacb1efcf5df597e5005cd7603b10787de1
Leonardo Lazzaro <lazzaroleonardo@gmail.com>
2013-07-09 11:42:08 -0300

    test for check duplicate results

--- tests/test_models.py
+++ tests/test_models.py
@@ -20,7 +20,7 @@ class TestModels(TestCase):
                 text="sarasa sarasa sarasa sarasa sarasa!",
                 name="test01",
                 month="Mar",
-                year=2013
+                year="2013"
                 )
         odm_session.flush()
         from_db = Document.query.get(name="test01")
@@ -36,7 +36,7 @@ class TestModels(TestCase):
                 text="sarasa sarasa sarasa sarasa sarasa!",
                 name="test02",
                 month="Mar",
-                year=2013
+                year="2013"
                 )
         InformationValueResult(
                 window_size=500,
@@ -49,3 +49,28 @@ class TestModels(TestCase):
         self.assertEquals(from_db.window_size, 500)
         self.assertEquals(from_db.document, simple_doc)
         self.assertEquals(from_db.results, [])
+
+    def test_simple_iv_result_persistence(self):
+        simple_doc = Document(
+                url="http://www.sarasa.com.ar",
+                text="sarasa sarasa sarasa sarasa sarasa!",
+                name="test02",
+                month="Mar",
+                year="2013"
+                )
+        InformationValueResult(
+                window_size=500,
+                document=simple_doc,
+                results=[]
+                )
+
+        odm_session.flush()
+        InformationValueResult(
+                window_size=500,
+                document=simple_doc,
+                results=[]
+                )
+
+        odm_session.flush()
+        count = InformationValueResult.query.find({"document_id":simple_doc._id}).count()
+        self.assertEquals(count, 1)
