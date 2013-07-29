0b58ca90ec50e6d61b75c46464978d52191caa53
julian3833 <julianpeller@gmail.com>
2013-07-28 22:18:35 -0300

    added .bak to .gitignore and rm config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-data_dir="/home/marat/Dropbox/Facu/incc/incc-2012/TP Lenin/data/"
-MIN_YEAR = 1900
-MAX_YEAR = 1905
-WNA_VERBOSE = 1
-NUMBER_OF_THREADS = 6
-SUM_THRESHOLD = 0.01
-
-
-DATABASE_NAME = 'lenin'
