c0724566af6c2323f0e3bc033efa4c25b110809f
julian3833 <julianpeller@gmail.com>
2013-08-23 21:14:46 -0300

    added iv-results to dump in other dir

--- get_dump.sh
+++ get_dump.sh
@@ -2,6 +2,6 @@
 #asume que borraste las colecciones de lenin, por ejemplo, ejecutando un test
 #mkdir dump
 #mkdir dump/lenin
-#wget ftp://lu000482:sakiraMI58@jmperez.com.ar/dump/lenin/* -P dump/lenin
+#wget ftp://lu000482:sakiraMI58@jmperez.com.ar/dump/lenin-with-iv/* -P dump/lenin
 cd dump
 mongorestore lenin
