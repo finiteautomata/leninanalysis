750516a1d74ce1f9cd61dfbb8d94b0ea6555d19c
Leonardo Lazzaro <lazzaroleonardo@gmail.com>
2013-08-29 11:31:45 -0300

    Delete get_dump.sh

--- get_dump.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-#asume que borraste las colecciones de lenin, por ejemplo, ejecutando un test
-#mkdir dump
-#mkdir dump/lenin
-#wget ftp://lu000482:sakiraMI58@jmperez.com.ar/dump/lenin-with-iv/* -P dump/lenin
-cd dump
-mongorestore lenin
