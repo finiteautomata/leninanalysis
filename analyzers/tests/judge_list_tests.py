5f5c73440d7b9a43ca3e0848882f9644f051b893
geekazoid <jmperez.85@gmail.com>
2013-11-26 18:30:32 -0300

    Add a bunch of judge_list tests.

--- analyzers/tests/judge_list_tests.py
+++ analyzers/tests/judge_list_tests.py
@@ -18,3 +18,40 @@ class JudgeListTests(TestCase):
 
         self.assertAlmostEqual(wna.judge_list(doc_list), 1.0)
 
+
+    def test_returns_0_for_document_with_one_distinct_top_word(self):
+        wna = WordNetAnalyzer("war")
+        doc_list = create_document_list(
+            document_returning_top_words(("music", 1.0))
+        )
+        self.assertAlmostEqual(wna.judge_list(doc_list), .0)
+
+    def test_returns_the_distance_ponderated_with_the_top_word_value(self):
+        wna = WordNetAnalyzer("war")
+        doc_list = create_document_list(
+            document_returning_top_words(("music", .5), ("war", 0.5))
+        )
+        self.assertAlmostEqual(wna.judge_list(doc_list), .5)
+
+    def test_with_two_documents(self):
+        wna = WordNetAnalyzer("war")
+        doc_list = create_document_list(
+            document_returning_top_words(("war", 1.0)),
+            document_returning_top_words(("music", 1.0))
+        )
+
+        self.assertAlmostEqual(wna.judge_list(doc_list), 0.5)
+
+    def test_with_two_documents_with_several_top_words(self):
+        wna = WordNetAnalyzer("war")
+        doc_list = create_document_list(
+            # Both docs adds 0.25 of similarity
+            document_returning_top_words(("war", 1.0)),
+            document_returning_top_words(("war", 1.0)),
+            # This one adds 
+            document_returning_top_words(("action", 0.5), ("war", 0.25), ("fury", 0.5)),
+            # This is filtered because its value is less than 0.4
+            document_returning_top_words(("action", 0.25))
+        )
+
+        self.assertAlmostEqual(wna.judge_list(doc_list), 0.625)
