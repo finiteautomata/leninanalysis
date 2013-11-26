3ac98ad7c43e4a8716420ea4100b47c3a5230aa0
geekazoid <jmperez.85@gmail.com>
2013-11-25 23:46:04 -0300

    Add new test to year analyzer.

--- analyzers/tests/year_analyzer_tests.py
+++ analyzers/tests/year_analyzer_tests.py
@@ -9,7 +9,7 @@ class YearAnalyzerTest(TestCase):
         YearAnalyzer(concepts=["war", "toilette", "mathemathics"])
 
     @patch('analyzers.year_analyzer.Document')
-    def test_get_year_analysis_for_empty_year_returns_concepts_in_zero(self, document_class_mock):
+    def test_returns_concept_in_zero_for_no_documents(self, document_class_mock):
         analyzer = YearAnalyzer(concepts=["war", "toilette"])
         document_class_mock.query.find.return_value = query_returning([])
         
@@ -17,9 +17,28 @@ class YearAnalyzerTest(TestCase):
 
         self.assertDictEqual({'toilette':.0, 'war':.0}, ret)
 
+    @patch('analyzers.year_analyzer.Document')
+    def test_returns_no_concepts_if_none_brought(self, document_class_mock):
+        analyzer = YearAnalyzer(concepts=[])
+        document_class_mock.query.find_return_value = query_returning([
+            document_returning_top_words(("war", .3), ("toilette", .4), ("music", 0.3))
+        ])
+
+        ret = analyzer.analyze_year(1899)
+
+        self.assertDictEqual({}, ret)
+
+
+
 def query_returning(docs):
     query = Mock()
     query.__iter__ = Mock()
     query.__iter__.return_value = iter(docs)
     query.count.return_value = len(docs)
     return query
+
+
+def document_returning_top_words(*ponderated_words):
+    doc = Mock()
+    doc.top_words.return_value = ponderated_words
+    return doc
