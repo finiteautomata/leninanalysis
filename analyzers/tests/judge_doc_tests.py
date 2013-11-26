7ea742ed83633250e37a786ecd5137675a83ef1c
geekazoid <jmperez.85@gmail.com>
2013-11-25 22:23:46 -0300

    Add judge doc tests.

--- analyzers/tests/judge_doc_tests.py
+++ analyzers/tests/judge_doc_tests.py
@@ -18,3 +18,11 @@ class JudgeDocTests(TestCase):
     
         self.assertAlmostEqual(wna.judge_doc(doc), .0)
 
+    def test_judge_doc_should_return_1_for_a_document_with_two_equal_words(self):
+        wna = WordNetAnalyzer("war")
+        doc = Mock()
+        doc.top_words.return_value = [("war", .5), ("war", .5)]
+    
+        self.assertAlmostEqual(wna.judge_doc(doc), 1.0)
+
+
