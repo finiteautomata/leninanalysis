e1c9e7841bfa46dea67d26440b57436dc999706e
geekazoid <jmperez.85@gmail.com>
2013-12-09 23:40:27 -0300

    Save many similarities in db.

--- analyzers/document_analyzer.py
+++ analyzers/document_analyzer.py
@@ -1,30 +1,42 @@
 #! encoding:utf-8
-from wn_analyzer import wna_for
+from wn_analyzer import WordNetAnalyzer
 import logging
+from similarity import path_similarity
 from interpreter import db
 
 log = logging.getLogger('lenin')
 
 class DocumentAnalyzer(object):
-    def __init__(self, concepts):
+    """
+    @concepts = The words that are going to be analyzed 
+    @similarity_function = A similarity function from similarity module
+    """
+    def __init__(self, concepts, similarity_function=path_similarity):
         self.concepts = concepts
-        self.analyzers = dict((concept, wna_for(concept)) for concept in concepts)
+        self.similarity_function = similarity_function
+        self.prefix = similarity_function.func_name
+        self.analyzers = dict((concept, WordNetAnalyzer(concept, similarity_function=similarity_function)) for concept in concepts)
  
     def analyze_document(self, document):
-        document_analysis = db.document_analysis.find_one({"document_id": document._id}) or {"document_id": document._id}
-        
+        document_analysis = self.__get_analysis_for(document)
+
         for concept in self.concepts:
-            self.__analyze_document_against_concept(concept=concept,
-                document=document,
-                document_analysis=document_analysis)
+            document_analysis[self.prefix][concept] = self.__analyze_document_against_concept(concept=concept,
+                document=document, document_analysis=document_analysis)
         
         db.document_analysis.save(document_analysis)
+        return document_analysis[self.prefix]
+
+    def __get_analysis_for(self, document):
+        document_analysis = db.document_analysis.find_one({"document_id": document._id}) or {"document_id": document._id}
+        if not self.prefix in document_analysis.keys():
+            document_analysis[self.prefix] = {}
         return document_analysis
 
     def __analyze_document_against_concept(self, document, concept, document_analysis):
         # Only calculate in case there's no analysis done
-        if concept in document_analysis.keys():
-            return
+        if concept in document_analysis[self.prefix].keys():
+            return document_analysis[self.prefix][concept]
         analyzer = self.analyzers[concept]
-        document_analysis[concept] = analyzer.judge_doc(document)
+        return analyzer.judge_doc(document)
     
--- analyzers/similarity/similarities.py
+++ analyzers/similarity/similarities.py
@@ -1,6 +1,8 @@
 #! coding:utf-8
 from nltk.corpus import wordnet as wn
 from synsets import get_word_synsets
+    
+
 
 
 def path_similarity(word1, word2):
@@ -8,5 +10,12 @@ def path_similarity(word1, word2):
     synsets2 = get_word_synsets(word2)
 
     similarities = [wn.path_similarity(ss1, ss2) for ss1 in synsets1 for ss2 in synsets2]
+    return max(similarities) if len(similarities) != 0 else .0
+
+def lch_similarity(word1, word2):
+    synsets1 = get_word_synsets(word1)
+    synsets2 = get_word_synsets(word2)
+
+    similarities = [wn.lch_similarity(ss1, ss2) for ss1 in synsets1 for ss2 in synsets2]
     return max(similarities) if len(similarities) != 0 else .0
 
