1380ae0c4809c8fa6431c2273eb5ceb350e06350
Leonardo Lazzaro <lazzaroleonardo@gmail.com>
2014-03-01 20:48:19 -0300

    some improvement on graphs. added against_to. compare against politics, economy and philosophy. also add graph that compares out chosen synsets

--- commands/top_senses_tables.py
+++ commands/top_senses_tables.py
@@ -126,7 +126,7 @@ def create_analysis_tables(doc_list):
             analysis_output.write("""\end{tabular}\n\end{center}""")
 
 
-def create_graph_top_senses(doc_list):
+def create_graphs(doc_list):
     documents = doc_list
     if documents is None:
         documents = default_document_list()
@@ -142,7 +142,54 @@ def create_graph_top_senses(doc_list):
     for doc in documents:
         for sense in doc.top_senses():
             all_senses.append((sense, doc.name))
+    against_colors = ['r', 'b', 'g']
+    against_to = [wn.synset(word) for word in ["economy.n.01", "philosophy.n.02", "politics.n.01"]]
+    create_against_graph('phyl_eco_pol', documents, all_senses, against_to, distance_functions, against_colors)
 
+    against_to = SYNSETS
+
+    against_colors = [(random(), random(), random()) for _i in range(0, len(SYNSETS))]
+    create_against_graph('handpicked', documents, all_senses, against_to, distance_functions, against_colors)
+
+    create_graph_top_senses(documents, all_senses, distance_functions)
+
+
+def create_against_graph(file_prefix, documents, all_senses, against_to, distance_functions, against_colors):
+    doc_senses = defaultdict(list)
+    node_list_against_to = [sense.name for sense in against_to]
+
+    for sense, doc_name in all_senses:
+        #this is used for give colors that corresponds to each document
+        doc_senses[doc_name].append(sense.name)
+
+    for function_name, distance_function in distance_functions:
+        G = nx.Graph()
+        for sense, doc_name in all_senses:
+            G.add_node(sense.name)
+        for node_against_name in node_list_against_to:
+            G.add_node(node_against_name)
+
+        for sense_1, doc_name in all_senses:
+            for sense_2 in against_to:
+                if sense_1 != sense_2:
+                    G.add_edge(sense_1.name, sense_2.name, weight=distance_function(sense_1, sense_2))
+
+        pos = nx.spring_layout(G, iterations=50)
+        # lets put the colors for each doc
+        colors = [(random(), random(), random()) for _i in range(0, len(doc_senses.keys()))]
+        for index, doc_name in enumerate(doc_senses):
+            nx.draw_networkx_nodes(G, pos, node_size=60, linewidths=0, nodelist=doc_senses[doc_name], node_color=colors[index])
+        for index, sense_name in enumerate(node_list_against_to):
+            nx.draw_networkx_nodes(G, pos, node_size=120, linewidths=0, nodelist=[sense_name], node_color=against_colors[index])
+
+        G.clear()
+        #plt.axis('off')
+        plt.savefig("paper/%s_%s_sim_graph.png" % (file_prefix, function_name))  # save as png
+        plt.show()  # display
+        plt.clf()
+
+
+def create_graph_top_senses(documents, all_senses, distance_functions):
     doc_senses = defaultdict(list)
 
     for sense, doc_name in all_senses:
@@ -152,6 +199,7 @@ def create_graph_top_senses(doc_list):
         G = nx.Graph()
         for sense, doc_name in all_senses:
             G.add_node(sense.name)
+            G.add_node(sense.name)
 
         for sense_1, doc_name in all_senses:
             for sense_2, doc_name in all_senses:
@@ -159,12 +207,10 @@ def create_graph_top_senses(doc_list):
                     G.add_edge(sense_1.name, sense_2.name, weight=distance_function(sense_1, sense_2))
 
         pos = nx.spring_layout(G, iterations=50)
-        # lets put the colors for each doc
         colors = [(random(), random(), random()) for _i in range(0, len(doc_senses.keys()))]
         for index, doc_name in enumerate(doc_senses.keys()):
-            nx.draw_networkx_nodes(G, pos, node_size=10, nodelist=doc_senses[doc_name], node_color=colors[index])
+            nx.draw_networkx_nodes(G, pos, node_size=60, linewidths=0, nodelist=doc_senses[doc_name], node_color=colors[index])
         G.clear()
-        plt.axis('off')
         plt.savefig("paper/%s_sim_graph.png" % function_name)  # save as png
         plt.show()  # display
         plt.clf()
@@ -173,4 +219,4 @@ def create_graph_top_senses(doc_list):
 def create_tables(doc_list):
 #    create_top_sense_tables(doc_list)
 #    create_analysis_tables(doc_list)
-    create_graph_top_senses(doc_list)
+    create_graphs(doc_list)
